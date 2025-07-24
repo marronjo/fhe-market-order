5731531988c43ced2c6f52bb1be9d1c5f8b9d5d7
Joe Marron <marronjo@tcd.ie>
2025-07-24 15:43:33 +0100

    update hook config

--- script/base/Config.sol
+++ script/base/Config.sol
@@ -10,7 +10,7 @@ contract Config {
     /// @dev populated with default anvil addresses
     IERC20 constant token0 = IERC20(address(0x2f4eD4942BdF443aE5da11ac3cAB7bee8d6FaF45));       // CPH
     IERC20 constant token1 = IERC20(address(0xbD313aDE73Cc114184CdBEf96788dd55118d4911));       // MSK
-    IHooks constant hookContract = IHooks(address(0x26ec265d869e4B024Eb0161961d5f72B31b20080)); // Market Order Hook
+    IHooks constant hookContract = IHooks(address(0x31f5b2DbC1497fA726C0240417E6E2c6089EC080)); // Market Order Hook
 
     Currency constant currency0 = Currency.wrap(address(token0));
     Currency constant currency1 = Currency.wrap(address(token1));
